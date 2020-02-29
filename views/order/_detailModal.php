9b73895a244a9777f148b0cc90b451836e4fa6fe
ademola2 <ademola@omnibiz.com>
2020-02-29 13:47:33 +0100

    create a modal to display more details

---
+++ views/order/_detailModal.php
@@ -0,0 +1,41 @@
+<!-- Order Details Modal -->
+<div id="detailModal" class="modal fade" id="user1" tabindex="-1" role="dialog" aria-labelledby="user1" aria-modal="true">
+	  <div class="modal-dialog modal-dialog-scrollable" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header text-center">
+	        <h5 class="modal-title" id="user1title"></h5>
+	        <button type="button" class="close closeDetailModal" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+	      <div class="modal-body">
+	        <div class="order-info">
+	        	<div class="order-detail">
+	        		<i class="fa fa-envelope-o"></i> <span id="orderEmail"></span>
+	        	</div>
+	        	<div class="order-detail">
+	        		<i class="fa fa-phone"></i> <span id="orderPhone"></span>
+	        	</div>
+	        	<div class="order-detail">
+	        		<i class="fa fa-truck"></i> <span id="orderType"></span>
+	        	</div>
+	        	<div class="order-detail">
+	        		<i class="fa fa-money"></i> <span id="orderAmount"></span>
+                </div>
+	        	<div class="order-detail">
+	        		<i class="fa fa-calendar"></i> <span id="orderSchedleDate"></span>
+                </div>
+                <div class="order-detail">
+	        		<i class="fa fa-tasks"></i> <span id="orderStatus"></span>
+	        	</div>
+	        	<div class="order-detail">
+	        		<i class="fa fa-map-marker"></i> <span id="orderAddress"> </span>
+	        	</div>
+	        	<div class="order-detail">
+	        		<i class="fa fa-globe"></i> <span id="orderCountry"></span>
+	        	</div>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	</div>
