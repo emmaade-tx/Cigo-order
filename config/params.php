9803a4d12cd742f34bbc994177109b0193492d06
ademola2 <ademola@omnibiz.com>
2020-03-01 10:48:20 +0100

    add api token to param

--- config/params.php
+++ config/params.php
@@ -4,4 +4,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+    'geocodeApiKey' => '43d28b79857ee554d2b779518e57242ebee75d9'
 ];
