36bb091fbbe454d7133382ae438f34905a70297d
ademola2 <ademola@omnibiz.com>
2020-03-01 11:23:32 +0100

    update db for heroku

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=cygo',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=wp433upk59nnhpoh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=o02ge5sybuukzwsn',
+    'username' => 'juq32of4gc05bthw',
+    'password' => 'amdw5p8hrep4ws5u',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
